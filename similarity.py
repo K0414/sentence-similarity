53bb6e0713384b36ef03cf7a2c6acc72b2f13c44
K0414 <liufc10@gmail.com>
2014-08-17 16:39:27 +0800

    tfidf version.

--- similarity.py
+++ similarity.py
@@ -1,20 +1,59 @@
 import sys
 import jieba
+import operator
+import math
 
-quest_dict = []
+doc_dict = []
+doc_vectors = []
+dfs = {}
+tfidfs = {}
+
+def calc_df(documents):
+    for d in documents:
+        for w in set(d):
+            if w not in dfs:
+                dfs[w] = 1
+            else:
+                dfs[w] += 1
 
 def method_cosine(question):
+    # 1. Word segmentation and stop words filtering.
+    # 2. Extract descriptive words using tf-idf.
+    # 3. Build vectors for question and each sentence in dictionary.
+    # 4. Calculate cosine distance.
+    # 5. Get TopK candidates.
     pass
 
-def find_similar_questions(question):
-    #return [ question ]
-    return jieba.cut(question)
+def method_tfidf(question):
+    def tfidf(quest, doc):
+        tf = {}
+        for w in quest:
+            tf[w] = 0.0
+        for w in doc:
+            if w in tf:
+                tf[w] += 1.0
+        tfidf = 0.0
+        for k in tf.keys():
+            df = (k in dfs) and dfs[k] or 0.0
+            tfidf += (tf[k] / len(doc)) * math.log(len(doc_dict) / (1 + df))
+        return tfidf
+    # 1. Word segmentation and stop words filtering.
+    quest_vector = [x for x in jieba.cut(question)]
+    # 2. Calculate tfidf for each document.
+    for idx, doc in enumerate(doc_vectors):
+        tfidfs[idx] = tfidf(quest_vector, doc)
+    # 3. Get Top5 candidates.
+    candidates = sorted(tfidfs.iteritems(), key=operator.itemgetter(1))[-5:]
+    return candidates[::-1]
 
-def load_quest_dict(filename):
+def find_similar_questions(question):
+    return method_tfidf(question)
+
+def load_doc_dict(filename):
     f = open(filename)
     l = f.readline()
     while l:
-        quest_dict.append(l)
+        doc_dict.append(l)
         l = f.readline()
     f.close()
 
@@ -27,13 +66,20 @@ if __name__=='__main__':
 
     # Load question dictionary.
     try:
-        load_quest_dict(sys.argv[1])
+        load_doc_dict(sys.argv[1])
     except IOError:
         print 'Open dictionary file failed.'
         sys.exit(-1)
 
+    # Cut sentences into word vectors.
+    for q in doc_dict:
+        doc_vectors.append([ x for x in jieba.cut(q)])
+
+    # Calculate DF of each word in the dictionary.
+    calc_df(doc_vectors)
+
     while True:
         question = raw_input('Input your question: ')
         candidates = find_similar_questions(question)
-        for c in candidates:
-            print c
+        for i, c in enumerate(candidates):
+            print 'Doc%d: %f, %s' % (i+1, c[1], doc_dict[c[0]])
